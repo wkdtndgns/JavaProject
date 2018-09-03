2336bb2272da351024f3b008f2d69c7a261d1685
wkdtndgns <wkdtndgns@naver.com>
2018-09-03 13:01:26 +0900

    jvm 구조 탐색

---
+++ src/Person/Person01.java
@@ -0,0 +1,13 @@
+package Person;
+
+public class Person01 {
+
+    public static void main(String[] args) {
+        Person p1 = new Person("홍길동", 18);
+        Person p2 = new Person("홍길동", 18);
+        
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p1.equals(p2));
+    }
+}
