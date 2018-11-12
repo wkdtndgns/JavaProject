c04466104192e321e7db1789687581130535acc9
wkdtndgns <wkdtndgns@naver.com>
2018-11-12 13:04:39 +0900

    구현 실습

--- src/Map/lecture.java
+++ src/Map/lecture.java
@@ -1,6 +1,8 @@
 package Map;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -13,12 +15,12 @@ public class lecture {
 		scanner.useDelimiter("[^a-zA-Z]+");
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		
-		while (scanner.hasNext("[a-zA-Z]+")) {
+		while (scanner.hasNext()) {
 			String s = scanner.next();
 			for(int i=0; i<s.length();i++) {
 				String key = String.valueOf(s.charAt(i));
 				if(map.get(key)==null) {
-					map.put(key, 0);}
+					map.put(key, 1);}
 				else {
 					int temp = map.get(key);
 					map.remove(key);
@@ -27,7 +29,32 @@ public class lecture {
 			}			
 		}
 		
-		System.out.println();
+		ArrayList<String> al = new ArrayList<String>();
+		
+		String[] arr = map.toString().replaceAll("[\\{\\}]"," ").split(",");
+		
+//내림차순 짜는중
+		for(int i=0; i<arr.length; i++) {
+			al.add(arr[i]);
+		}		
+		al.sort(new MapComparator());
+
+		
+		System.out.println(map.toString());
+
+		System.out.println("내림차순");
+		System.out.println(al);
 		scanner.close();
 	}
+}
+
+class MapComparator implements Comparator<String>  {
+
+	@Override
+	public int compare(String s1, String s2) {
+		String[] arr1=s1.split("=");
+		String[] arr2=s2.split("=");
+		return Integer.parseInt(arr2[1].trim())-Integer.parseInt(arr1[1].trim());
+	}
+	
 }
