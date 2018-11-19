89799185fc02009c01e29a76af6e30a1eb82433f
wkdtndgns <wkdtndgns@naver.com>
2018-11-19 16:22:00 +0900

    실습 2번쨰

--- src/regular/lecture02.java
+++ src/regular/lecture02.java
@@ -28,12 +28,12 @@ public class lecture02 {
     public static void main(String[] args) throws IOException {        
         String path = "c:/홈페이지_게시판.html";
         String s = readTextFile(path);
-        
-        String regex = "<a href='boardread.aspx\\?idx=([0-9]+)[^>]+>([^<]+)</a>";
-        Pattern pattern = Pattern.compile(regex);
+        System.out.println(s);
+    	String regex = "<a href=\"http://www.skhu.ac.kr/board/boardread.aspx\\?idx=([0-9]+)[^>]+>(?<title>[^<]+)</a>";
+    	Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(s);
         while (matcher.find())
-                System.out.printf("%s %s\n", matcher.group(1), matcher.group(2));
+                System.out.printf("%s  %s \n", matcher.group(1), matcher.group(2).replaceAll("\\.", ""));
 
     }
 
